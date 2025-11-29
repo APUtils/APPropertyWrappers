d2c0361b80f220f49c2eceb3e66a359ee589a497
Anton Plebanovich <anton.plebanovich@gmail.com>
2025-11-29 12:26:12 +0600

    - [FilePreserved] `defaultValue` and `preserveDefault` params; Store plain value in `storage`

--- APPropertyWrappers/Core/Simple/FilePreserved.swift
+++ APPropertyWrappers/Core/Simple/FilePreserved.swift
@@ -17,80 +17,107 @@ open class FilePreserved {
     
     static let documentsURL: URL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).last!
     private let url: URL
+    private let preserveDefault: Bool
     private let setTransform: Transform
     private let getTransform: Transform
     
     /// Storage that is used to prevent continuous object read from file and so to speedup property access.
     private var storage: String?
+    private var defaultValue: String?
     
     open var wrappedValue: String? {
         get {
-            getTransform(storage)
+            storage
         }
         set {
-            let newValue = setTransform(newValue)
             guard newValue != storage else { return }
-            
             storage = newValue
-            if let newValue = newValue {
-                if let data = newValue.data(using: .utf8) {
-                    do {
-                        try data.write(to: url)
-                    } catch {
-                        RoutableLogger.logError("Unable to set new value. Data write failed.", error: error, data: ["url": url, "data": data])
-                    }
-                } else {
-                    RoutableLogger.logError("Unable to set new value. Data conversion failed.", data: ["url": url])
-                }
-            } else {
-                if FileManager.default.fileExists(atPath: url.path) {
-                    do {
-                        try FileManager.default.removeItem(at: url)
-                    } catch {
-                        RoutableLogger.logError("Unable to remove existing file. Preserved data was not removed.", error: error, data: ["url": url])
-                    }
-                }
-            }
+            
+            let value = setTransform(newValue)
+            Self.setValue(value, url: url)
         }
     }
     
     public init(key: String,
+                defaultValue: String? = nil,
+                preserveDefault: Bool = false,
                 setTransform: @escaping Transform = { $0 },
                 getTransform: @escaping Transform = { $0 }) {
         
         self.url = Self.documentsURL.appendingPathComponent("FilePreserved_\(key)")
+        self.preserveDefault = preserveDefault
+        self.defaultValue = defaultValue
         self.setTransform = setTransform
         self.getTransform = getTransform
         
-        if FileManager.default.fileExists(atPath: url.path) {
-            let data: Data
-            do {
-                data = try Data(contentsOf: url)
-            } catch {
-                RoutableLogger.logError("Unable to get data from existing file. Preserved data was not restored.", error: error, data: ["key": key, "url": url])
-                return
-            }
-            
-            if let string = String(data: data, encoding: .utf8) {
-                storage = string
-            } else {
-                RoutableLogger.logError("Unable to decode preserved data. Preserved data was not restored.", data: ["key": key, "url": url, "data": data])
-            }
-            
+        if let value = Self.getValue(url: url) {
+            self.storage = getTransform(value)
         } else {
-            storage = nil
+            if preserveDefault {
+                let value = setTransform(defaultValue)
+                Self.setValue(value, url: url)
+            }
+            self.storage = defaultValue
         }
     }
     
     /// Resets value to its default by erasing related file.
     public func reset() {
-        storage = nil
+        storage = defaultValue
+        
+        if preserveDefault {
+            let value = setTransform(defaultValue)
+            Self.setValue(value, url: url)
+            return
+        }
+        
         guard FileManager.default.fileExists(atPath: url.path) else { return }
         
         do {
             try FileManager.default.removeItem(at: url)
         } catch {
             RoutableLogger.logError("Unable to reset preserved data", error: error, data: ["url": url])
+        }
+    }
+    
+    fileprivate static func getValue(url: URL) -> String? {
+        guard FileManager.default.fileExists(atPath: url.path) else { return nil }
+        
+        let data: Data
+        do {
+            data = try Data(contentsOf: url)
+        } catch {
+            RoutableLogger.logError("Unable to get data from existing file. Preserved data was not restored.", error: error, data: ["url": url])
+            return nil
+        }
+        
+        if let string = String(data: data, encoding: .utf8) {
+            return string
+        } else {
+            RoutableLogger.logError("Unable to decode preserved data. Preserved data was not restored.", data: ["url": url, "data": data])
+            return nil
+        }
+    }
+    
+    fileprivate static func setValue(_ value: String?, url: URL) {
+        if let value {
+            if let data = value.data(using: .utf8) {
+                do {
+                    try data.write(to: url)
+                } catch {
+                    RoutableLogger.logError("Unable to set new value. Data write failed.", error: error, data: ["url": url, "data": data])
+                }
+            } else {
+                RoutableLogger.logError("Unable to set new value. Data conversion failed.", data: ["url": url])
+            }
+        } else {
+            if FileManager.default.fileExists(atPath: url.path) {
+                do {
+                    try FileManager.default.removeItem(at: url)
+                } catch {
+                    RoutableLogger.logError("Unable to remove existing file. Preserved data was not removed.", error: error, data: ["url": url])
+                }
+            }
         }
     }
 }
